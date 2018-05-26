bfe64137184ce49e5d4be47ebc5a2842f93346b0
Vassili Baranov <vassili.baranov@gmail.com>
2018-05-26 11:45:37 +0300

    create parse_line method

--- src/log_analyzer.py
+++ src/log_analyzer.py
@@ -18,8 +18,39 @@ config = {
 
 
 class LogAnalyzer:
+    cols_regexp = {
+        'remote_addr': r'[\d\.]+\s',
+        'remote_user': '\S*\s+',
+        'http_x_real_ip': r'\S*\s',
+        'time_local': r'\[.*?\]\s',
+        'request': r'".*?"\s',
+        'status': r'\d+\s',
+        'body_bytes_sent': r'\d+\s',
+        'http_referer': r'".*?"\s',
+        'http_user_agent': r'".*?"\s',
+        'http_x_forwarded_for': r'".*?"\s',
+        'http_X_REQUEST_ID': r'".*?"\s',
+        'http_X_RB_USER': r'".*?"\s',
+        'request_time': r'\d+\.\d+',
+    }
+
     def __init__(self, config):
         self.config = config
+        self.line_cols = (
+            'remote_addr',
+            'remote_user',
+            'http_x_real_ip',
+            'time_local',
+            'request',
+            'status',
+            'body_bytes_sent',
+            'http_referer',
+            'http_user_agent',
+            'http_x_forwarded_for',
+            'http_X_REQUEST_ID',
+            'http_X_RB_USER',
+            'request_time',
+        )
 
     def date_in_logname(self, logname):
         """ Returns datetime object with date from logname. """
@@ -39,6 +70,31 @@ class LogAnalyzer:
             logname = max(logs, key=self.date_in_logname)
             logpath = os.path.join(self.config['LOG_DIR'], logname)
             return logpath
+
+    def parse_line(self, line: str):
+        """ Parses one line from log. Returns dict. """
+        if not isinstance(line, str):
+            raise TypeError('line must be a string, but get %s' % type(line))
+
+        parsed_dict = {}
+        start = 0
+
+        for col in self.line_cols:
+            match = re.match(self.cols_regexp[col], line[start:])
+
+            if not match:
+                msg = 'Cannot parse %s in line \'%s\'' % (col, line)
+                raise ValueError(msg)
+
+            start += match.end()
+            value = match.group().strip('[]"" ')
+            if col in ('status', 'body_bytes_sent'):
+                value = int(value)
+            elif col == 'request_time':
+                value = float(value)
+            parsed_dict[col] = value
+
+        return parsed_dict
 
 
 def main():
---
+++ src/tests/test_log_analyzer.py
@@ -0,0 +1,93 @@
+import unittest
+from datetime import datetime
+from unittest import mock
+
+from ..log_analyzer import LogAnalyzer
+
+
+class TestLogAnalyzer(unittest.TestCase):
+    def setUp(self):
+        config = {
+            'REPORT_SIZE': 1000,
+            'REPORT_DIR': './reports',
+            'LOG_DIR': './log',
+            'LOG_PREFIX': 'sample',
+        }
+        self.analyzer = LogAnalyzer(config)
+        self.maxDiff = None
+
+    def test_date_in_logname(self):
+        logname = 'sample.log-20170630.gz'
+        expected = datetime(2017, 6, 30)
+        result = self.analyzer.date_in_logname(logname)
+        self.assertEqual(expected, result)
+
+    def test_date_in_logname_without_date(self):
+        logname = 'sample.log'
+        self.assertIsNone(self.analyzer.date_in_logname(logname))
+
+    def test_get_last_log(self):
+        with mock.patch('os.listdir') as mock_listdir:
+            mock_listdir.return_value = [
+                'sample.log-20170630.gz',
+                'sample.log-20170712.gz',
+                'sample.log-20170930',
+                'sample.log',
+                'nginx-any.log-20170630.gz'
+            ]
+            self.assertEqual(self.analyzer.get_last_log(),
+                             './log/sample.log-20170930')
+
+    def test_get_last_log_without_right_log(self):
+        with mock.patch('os.listdir') as mock_listdir:
+            mock_listdir.return_value = [
+                'sample.log',
+                'something-else.log',
+                'may_be_any',
+            ]
+            self.assertIsNone(self.analyzer.get_last_log())
+
+    def test_parse_line(self):
+        line = ('1.99.174.176 3b81f63526fa8  - [29/Jun/2017:03:50:22 +0300]'
+                ' "GET /api/1/photogenic_banners/list/?server_name=WIN7RB4'
+                ' HTTP/1.1" 200 12 "-" "Python-urllib/2.7" "-"'
+                ' "1498697422-32900793-4708-9752770" "-" 0.133')
+        expected = {
+            'remote_addr': '1.99.174.176',
+            'remote_user': '3b81f63526fa8',
+            'http_x_real_ip': '-',
+            'time_local': '29/Jun/2017:03:50:22 +0300',
+            'request': 'GET /api/1/photogenic_banners/list/?server_name=WIN7RB4 HTTP/1.1',
+            'status': 200,
+            'body_bytes_sent': 12,
+            'http_referer': '-',
+            'http_user_agent': 'Python-urllib/2.7',
+            'http_x_forwarded_for': '-',
+            'http_X_REQUEST_ID': '1498697422-32900793-4708-9752770',
+            'http_X_RB_USER': '-',
+            'request_time': 0.133,
+        }
+        result = self.analyzer.parse_line(line)
+        self.assertDictEqual(expected, result)
+
+    def test_parse_line_without_str(self):
+        cases = ({}, 123, 1.1, [], set())
+        for case in cases:
+            with self.subTest(case=case):
+                with self.assertRaises(TypeError):
+                    self.analyzer.parse_line(case)
+
+    def test_parse_line_with_bad_line(self):
+        cases = (
+            '',
+            'asdf vaasd vsdf',
+            '1.22.234.122 1fq12   - - - - - - - -'
+        )
+        for case in cases:
+            with self.subTest(case=case):
+                with self.assertRaises(ValueError):
+                    self.analyzer.parse_line(case)
+
+
+if __name__ == '__main__':
+    unittest.main()
